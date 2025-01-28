9634481495e27ddec8a01acf41909395b0fca812
Oleg_Ischuk <ipz231_ios@student.ztu.edu.ua>
2025-01-28 10:33:00 +0200

    Виконане завдання 1.

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"Sum {number1} + {number2} = {sum}.");
+        }
+    }
+}
